2c17f2e5e38b4e5c6633405688041ed5950f3f36
Dollyn <dollyn.sun@gmail.com>
2011-08-19 17:34:05 +0800

    t2

---
+++ python/django/mysite/polls/admin.py
@@ -0,0 +1,9 @@
+'''
+Created on 2011-8-19
+
+@author: sundl
+'''
+from polls.models import Poll
+from django.contrib import admin
+
+admin.site.register(Poll)
--- python/django/mysite/settings.py
+++ python/django/mysite/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'D:/workspaces/python/django/mysite/db.sqllite',                      # Or path to database file if using sqlite3.
+        'NAME': 'D:\workspaces\dollynprojects\python\django\mysite\db.sqllite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -117,7 +117,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'polls',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
