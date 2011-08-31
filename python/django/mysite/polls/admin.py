29926cf45e7ed423211e6d58c1e0ee502f9a004c
Dollyn <dollyn.sun@gmail.com>
2011-08-31 16:44:26 +0800

    none

--- python/django/mysite/polls/admin.py
+++ python/django/mysite/polls/admin.py
@@ -7,15 +7,17 @@ from polls.models import Poll
 from polls.models import Choice
 from django.contrib import admin
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 class PollAdmin(admin.ModelAdmin):
+    list_display = ('question', 'pub_date')
     fieldsets = [
         (None,               {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_filter = ['pub_date']
 
 admin.site.register(Poll, PollAdmin)
