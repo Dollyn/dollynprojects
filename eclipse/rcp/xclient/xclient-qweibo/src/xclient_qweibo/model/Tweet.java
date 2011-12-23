44ec77d153a167aebe22cbf18dd0aad37a968dab
Dollyn <dollyn.sun@gmail.com>
2011-12-23 16:34:35 +0800

    aa

--- eclipse/rcp/xclient/xclient-qweibo/src/xclient_qweibo/model/Tweet.java
+++ eclipse/rcp/xclient/xclient-qweibo/src/xclient_qweibo/model/Tweet.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class Info {
+public class Tweet {
 	public static final int ORIGINAL = 1;
 	public static final int RT = 2;
 	public static final int PRIVATE = 3;
@@ -25,31 +25,37 @@ public class Info {
 	private int mcount;
 	/** 来源 **/
 	private String from = "来自网页";
-
+	/** 图片URL列表 */
 	String[] image = new String[] { "", "" };
+	
+	private Video video;
+	private Music music;
 
 	// video:{picurl:""",player:"",realurl:"",shorturl:"",title:""},
 	// music:{author:"",url:"",title:""},
 	/** 发表人的帐户名 **/
-	private String name;
+	private String name;//发表人帐户名
 	/** 发表人昵称 */
-	private String nick;
-	String uid = "xxxxxx";
+	private String nick;//发表人昵称
+	String openid = "xxxxxx";//用户唯一id，与name相对应
 	// source:
 
-	int type;
-	int self;// 0--no; 1-- yes
+	int type;//微博类型，1-原创发表，2-转载，3-私信，4-回复，5-空回，6-提及，7-评论
+	int self;// 是否自已发的的微博，0-不是，1-是
 	
 	/** 发表时间 */
-	long timestamp;
-	String head;
-	String location;
-	String country_code;
-	String province_code;
-	String city_code;
-	int isvip;
-	String geo;
-	int status;
+	long timestamp;//发表时间
+	String head;//发表者头像url
+	
+	String location;//发表者所在地
+	String country_code;//国家码（其他时间线一样）
+	String province_code;//省份码（其他时间线一样）
+	String city_code;//城市码（其他时间线一样）
+	int isvip;//是否微博认证用户，0-不是，1-是
+	String geo;//发表者地理信息
+	int status;//微博状态，0-正常，1-系统删除，2-审核中，3-用户删除，4-根删除
 
-	Info source;
+	String emotionurl;//心情图片url
+	int emotiontype;//心情类型
+	Tweet source;//当type=2时，source即为源tweet
 }
--- eclipse/rcp/xclient/xclient-qweibo/src/xclient_qweibo/model/Video.java
+++ eclipse/rcp/xclient/xclient-qweibo/src/xclient_qweibo/model/Video.java
@@ -5,9 +5,16 @@ import javax.persistence.Id;
 
 @Entity
 public class Video {
+	
+	/** 缩略图 */
 	@Id
 	private String picurl;
+	/** 播放器地址 */
 	private String player;
+	/** 视频原地址 */
 	private String realurl;
+	/** 视频的短url */
 	private String shorturl;
+	/** 视频标题 */
+	private String title;	
 }
