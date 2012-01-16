7eec5942e4e564cefc60e3f1e6c9ee6d26a6e9cb
Dollyn <dollyn.sun@gmail.com>
2012-01-16 20:59:48 +0800

    my google reader

---
+++ chrome/MyGoogleReader/content.js
@@ -0,0 +1,11 @@
+
+function modify() {
+	$(document).ready(function() {
+		$(".folder-icon").css("background-image", "url(https://ssl.gstatic.com/docs/doclist/images/collectionsprite_1.png)");
+		$(".folder-icon").css("background-position", "-342px 0");
+		$(".folder-icon").css("opacity", "0.7");
+		$("#viewer-header").hide();
+	});
+
+}
+modify();
