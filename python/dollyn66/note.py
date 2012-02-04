04a18d48a5e812cee3bc7703b12817a920967d94
Dollyn <dollyn.sun@gmail.com>
2012-02-04 16:27:01 +0800

    Change-Id: I5647e7e1137368f42ad55af80841812e6b4037b1

--- python/dollyn66/note.py
+++ python/dollyn66/note.py
@@ -4,7 +4,7 @@ Created on 2011-10-19
 @author: sundl
 '''
 
-from google.appengine.api import users
+from google.appengine.api import users 
 from google.appengine.ext import db, webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
